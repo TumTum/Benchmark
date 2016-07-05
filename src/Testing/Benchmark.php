16f9cbc35f15ab7a2881adcec56146ca1ad8bdc6
Daryl Eisner <ucsdmath.project@gmail.com>
2016-07-05 08:45:36 -0700

    Update

--- src/Testing/Benchmark.php
+++ src/Testing/Benchmark.php
@@ -47,7 +47,7 @@ final class Benchmark implements BenchmarkInterface
      *
      * @api
      */
-    const VERSION = '1.7.0';
+    const VERSION = '1.8.0';
 
     //--------------------------------------------------------------------------
 

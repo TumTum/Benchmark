35bfcf8b2d26e75ba6bd985ca5daabef01b02b7c
UCSDMath <UCSDMath.Project@gmail.com>
2017-01-23 18:22:14 -0800

    Update

--- src/Testing/Benchmark.php
+++ src/Testing/Benchmark.php
@@ -47,7 +47,7 @@ final class Benchmark implements BenchmarkInterface
      *
      * @api
      */
-    const VERSION = '1.12.0';
+    const VERSION = '1.13.0';
 
     //--------------------------------------------------------------------------
 

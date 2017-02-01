c761508aeeac3c128ee962c11fed85a74aeda775
Tobias Matthaiou <tm@solutiondrive.de>
2017-02-01 15:17:27 +0100

    Test to save the same output

--- tests/BenchmarkTest.php
+++ tests/BenchmarkTest.php
@@ -19,6 +19,48 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test Standard Benchmark Output
+     */
+    public function testBenchmarkOutputAsHTMLComment()
+    {
+        $this->subject->start();
+        $actual = $this->subject->stop();
+
+        $expected[] = '';
+        $expected[] = '    <!-- ======== \[ BENCHMARK DATA \] ========';
+        $expected[] = '    Elapsed Time: 0ms';
+        $expected[] = '    Elapsed Time: [0-9E.-]+ \(micro\)';
+        $expected[] = '    Memory Usage: [0-9.-]+[B|Kb|Mb|Gb|Tb]+';
+        $expected[] = '     Peak Memory: [0-9.-]+[B|Kb|Mb|Gb|Tb]+ \(or [0-9]+ bytes\)';
+        $expected[] = '            Date: \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d';
+        $expected[] = '    ===================================== -->';
+
+        $this->assertLines($expected, $actual);
+    }
+
+    /**
+     * Test Standard Benchmark Output as Display
+     */
+    public function testBenchmarkOutputDisplayAsHTML()
+    {
+        $this->subject->start();
+        $actual = $this->subject->stop(true);
+
+        $expected[] = '';
+        $expected[] = '<pre id="bench">';
+        $expected[] = '======== \[ BENCHMARK DATA \] ========';
+        $expected[] = '    Elapsed Time: 0ms';
+        $expected[] = '    Elapsed Time: [0-9E.-]+ \(micro\)';
+        $expected[] = '    Memory Usage: [0-9.-]+[B|Kb|Mb|Gb|Tb]+';
+        $expected[] = '     Peak Memory: [0-9.-]+[B|Kb|Mb|Gb|Tb]+ \(or [0-9]+ bytes\)';
+        $expected[] = '            Date: \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d';
+        $expected[] = '    ===================================== ';
+        $expected[] = '<\/pre>';
+
+        $this->assertLines($expected, $actual);
+    }
+
+    /**
      * @param $expected
      * @param $actual
      */
